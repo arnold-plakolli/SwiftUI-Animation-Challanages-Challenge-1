afb39dba5dc6365524db3ea5ffa2cbe7dc045904
Arnold Plakolli <plakolliarnold@gmail.com>
2022-09-04 20:35:28 +0200

    completed challenge

---
+++ arnold-plakolli/SwiftUI-Animation-Challanages-Challenge-1/SwiftUI-Animation-Challanages-Challenge-1/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  SwiftUI-Animation-Challanages-Challenge-1
+//
+//  Created by Arnold Plakolli on 4.9.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	private struct Item: Identifiable {
+		let id: UUID = .init()
+		var value: Double
+	}
+	
+	private let size: CGSize = .init(width: 300, height: 300)
+	@State private var items: [Item] = [
+		.init(value: 0.1),
+		.init(value: 0.5),
+		.init(value: 0.75),
+		.init(value: 0.25)
+	]
+	private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+	
+	var body: some View {
+		ZStack {
+			Color.black
+			
+			HStack {
+				ForEach(items) { item in
+					ZStack {
+						ZStack(alignment: .bottom) {
+							Color.clear
+							Color.green
+								.cornerRadius(5)
+								.frame(height: size.height * item.value)
+						}
+						.frame(height: size.height)
+					}
+				}
+			}
+			.frame(width: size.width)
+		}
+		.ignoresSafeArea()
+		.onReceive(timer) { _ in
+			withAnimation {
+				for i in 0..<items.count {
+					items[i].value = Double.random(in: 0.1...1.0)
+				}
+			}
+		}
+	}
+}
+
+struct ContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		ContentView()
+	}
+}
---
+++ arnold-plakolli/SwiftUI-Animation-Challanages-Challenge-1/SwiftUI-Animation-Challanages-Challenge-1/SwiftUI_Animation_Challanages_Challenge_1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Animation_Challanages_Challenge_1App.swift
+//  SwiftUI-Animation-Challanages-Challenge-1
+//
+//  Created by Arnold Plakolli on 4.9.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Animation_Challanages_Challenge_1App: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
